457b6f21daae3f5a17daf8135526074bfd413150
DaianeTararam <daalkmim@gmail.com>
2023-03-05 15:58:53 -0300

    Initial commit

---
+++ src/edu/daiane/segundasemana/MinhaClasse.java
@@ -0,0 +1,9 @@
+package edu.daiane.segundasemana;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        System.out.print("Olá turma, sejam bem-vindos!");
+    }
+}
+
